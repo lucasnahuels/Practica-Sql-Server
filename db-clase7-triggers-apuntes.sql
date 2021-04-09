2b3ddb7b290332f396365b77bb240d7878c10ec4
Lucas Stirpe <49053857+lucasnahuels@users.noreply.github.com>
2021-04-09 05:29:17 -0300

    Update db-clase7-triggers-apuntes.sql

--- db-clase7-triggers-apuntes.sql
+++ db-clase7-triggers-apuntes.sql
@@ -6,7 +6,7 @@ use EMPRESA
 
 --Inserted y Deleted son dos tablas que se crean automaticamente cada vez que se dispara un trigger
 
---Inserted almacena cualquier fila que se vaya a añadir a la tabla
+--Inserted almacena cualquier fila que se vaya a aÃ±adir a la tabla
 --Deleted almacena cualquier fila que se vaya a borrar de la tabla
 
 drop trigger ingreso_stock;
@@ -16,16 +16,16 @@ drop trigger ingreso_stock;
 
 --Esto funciona a pesar del error que tira aca, seleccionar el bloque
 create TRIGGER ingreso_stock on producto FOR insert
-	AS
+AS
 begin
 	insert into stock(codigo_producto, cantidad, pto_reposicion)
-		select ins.codigo_producto, 0, 50 from inserted ins
+	select ins.codigo_producto, 0, 50 from inserted ins
 end
 
 --Para ejecutar el trigger, hago un insert en la tabla que tenga el trigger
 
 insert into producto(descripcion, precio_costo, codigo_fabricante)
-	values ('Motor trifasico 500C', 3563, 2);
+values ('Motor trifasico 500C', 3563, 2);
 
 --Deshabilitar / Habilitar un trigger
 
@@ -40,22 +40,23 @@ use EMPRESA
 select * from detalle_pedido
 
 create trigger control_pedidos on detalle_pedido
-	for insert
-		as
-			declare @stock int
-			select @stock = s.cantidad from stock s join inserted
-				on inserted.codigo_producto = s.codigo_producto
-				where s.codigo_producto = inserted.codigo_producto
-		if (@stock >= (select cantidad from inserted))
-			update stock set cantidad = s.cantidad - inserted.cantidad
-			from stock s join inserted
-				on inserted.codigo_producto = s.codigo_producto
-					where s.codigo_producto = inserted.codigo_producto
-		else
-		begin
-			raiserror('Hay menos productos en stock de los solicitados para el pedido', 16, 1)
-			rollback transaction
-		end
+for insert
+	as
+	declare @stock int
+	select @stock = s.cantidad from stock s 
+	join inserted on inserted.codigo_producto = s.codigo_producto
+	where s.codigo_producto = inserted.codigo_producto
+	
+	if (@stock >= (select cantidad from inserted))
+		update stock set cantidad = s.cantidad - inserted.cantidad
+		from stock s
+		join inserted on inserted.codigo_producto = s.codigo_producto
+		where s.codigo_producto = inserted.codigo_producto
+	else
+	begin
+		raiserror('Hay menos productos en stock de los solicitados para el pedido', 16, 1)
+		rollback transaction
+	end
 
 /*Se le puede poner o no el 'as begin' al trigger. Mejor ponerlo para encontrar mejor donde comienza y termina el trigger*/
 --Probamos el trigger
@@ -64,18 +65,23 @@ insert into detalle_pedido values(17, 3, 1001, 500);
 insert into detalle_pedido values(17, 4, 1005, 500);
 
 --Crear un trigger que grabe en la tabla auditoria_producto los valores anteriores del registro modificado en la tabla producto
-create table auditoria_producto (codigo_producto int,
-								descripcion varchar(50),
-								precio_costo money,
-								codigo_fabricante int,
-								fecha_actualizacion datetime,
-								usuario varchar(50))
+create table auditoria_producto 
+(
+	codigo_producto int,
+	descripcion varchar(50),
+	precio_costo money,
+	codigo_fabricante int,
+	fecha_actualizacion datetime,
+	usuario varchar(50)
+)
 
 create trigger modif_producto on producto for update as
-	insert into auditoria_producto(codigo_producto, descripcion, precio_costo, codigo_fabricante, fecha_actualizacion, usuario)
-	--Si lo traes de un select no hace falta un VALUES
-	select del.codigo_producto, del.descripcion, del.precio_costo, del.codigo_fabricante, getdate(), user_name()
-	from deleted del
+insert into auditoria_producto(codigo_producto, descripcion, precio_costo, codigo_fabricante, fecha_actualizacion, usuario)
+
+--Si lo traes de un select no hace falta un VALUES
+
+select del.codigo_producto, del.descripcion, del.precio_costo, del.codigo_fabricante, getdate(), user_name()
+from deleted del
 
 select * from producto
 select * from auditoria_producto
@@ -93,33 +99,40 @@ select * from oficina
 select * from empleado
 
 select codigo_oficina from oficina
-	where descripcion = 'Ventas Interior'
+where descripcion = 'Ventas Interior'
 
 --Se puede hacer sin una subconsulta, pero la idea es que las practiquemos
 --Se pueden hacer varias subconsultas anidades, pero habria que revisar la logica de la consulta, porque seria raro tener que hacer eso; capaz seria mejor hacer una vista de los datos, por ejemplo
 
 select apellido, nombre from empleado
-	where
-		codigo_oficina not in
-		(select codigo_oficina from oficina
-			where descripcion = 'Ventas Interior')
+where codigo_oficina not in
+(
+	select codigo_oficina from oficina
+	where descripcion = 'Ventas Interior'
+)
 
 --5. Listar sin repetir la razon social de aquellos clientes que hicieron por lo menos un pedido cuyo importe total sea mayor que $850
 select * from pedido
 select* from cliente
 select * from detalle_pedido
 select * from precio_venta /*codigo_lista significa que uno es mayorista y otro minorista. Dependiendo que cliente es le voy a cobrar un precio o otro*/
+
 --Aca tenemos dos niveles de anidamiento (2 subconsultas) Conviene hacerlas y probarlas desde el interior para afuera
+
 select razon_social from cliente
 	where codigo_cliente in
-	(select codigo_cliente from pedido where codigo_pedido in
-		(select pe.codigo_pedido from pedido pe, detalle_pedido dp, cliente c, precio_venta pv
+	(
+		select codigo_cliente from pedido where codigo_pedido in
+		(
+			select pe.codigo_pedido from pedido pe, detalle_pedido dp, cliente c, precio_venta pv
 			where pe.codigo_pedido = dp.codigo_pedido and
 				pe.codigo_cliente = c.codigo_cliente and
 				dp.codigo_producto = pv.codigo_producto and
 				pv.codigo_lista = c.codigo_lista /*codigo_lista y codigo_producto van de la mano ambas como pk de precio_venta*/
 			group by pe.codigo_pedido
-			having (sum(dp.cantidad*pv.precio)>850) ) )
+			having (sum(dp.cantidad*pv.precio)>850) 
+		) 
+	)
 order by razon_social
 
 --8. Listar sin repetir los productos que figuren con por lo menos una cantidad pedida mayor a 70 en el mes de marzo.
@@ -132,10 +145,10 @@ select descripcion from producto
 
 select distinct descripcion from producto
 where codigo_producto in(select dp.codigo_producto from detalle_pedido dp, pedido p
-							where month(p.fecha_pedido) = 3 and
-								p.codigo_pedido = dp.codigo_pedido
-								group by codigo_producto
-								having sum(cantidad)>70)
+where month(p.fecha_pedido) = 3 
+and p.codigo_pedido = dp.codigo_pedido
+group by codigo_producto
+having sum(cantidad)>70)
 								
 /*el select de la subconsulta debe ser igual al where de la consulta*/								
 /*el group by suele ser el mismo que el select*/
